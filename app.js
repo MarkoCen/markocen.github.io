be41e3b4feb0933e8efc20424aa65934170c6bd5
Marko Cen <markocen@gmail.com>
2015-03-30 23:41:14 -0400

    try HTML5Mode

--- app.js
+++ app.js
@@ -3,26 +3,30 @@
  */
 var markocen = angular.module('markocen', ['angularEndlessScrollBar','ngRoute','uiSlider','ngPopup']);
 
-markocen.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/angularEndlessScrollBar', {
-                templateUrl: 'angularEndlessScrollBar/angularEndlessScrollBarDemo.html',
-                controller: 'angularEndlessDemoCtrl'
-            }).
-            when('/ngPopup', {
-                templateUrl: 'ngPopup/ngPopupDemo.html',
-                controller: 'ngPopupCtrl'
-            }).
-            when('/colorful-buttons', {
-                templateUrl: 'colorful-buttons/demo.html',
-                controller: 'colorBtnCtrl'
-            }).
-            when('/about-me', {
-                templateUrl: 'aboutMe/about-me.html',
-                controller: 'aboutMeCtrl'
-            }).
-            otherwise({
-                redirectTo: '/about-me'
-            });
-    }]);
+markocen.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/angularEndlessScrollBar', {
+            templateUrl: 'angularEndlessScrollBar/angularEndlessScrollBarDemo.html',
+            controller: 'angularEndlessDemoCtrl'
+        }).
+        when('/ngPopup', {
+            templateUrl: 'ngPopup/ngPopupDemo.html',
+            controller: 'ngPopupCtrl'
+        }).
+        when('/colorful-buttons', {
+            templateUrl: 'colorful-buttons/demo.html',
+            controller: 'colorBtnCtrl'
+        }).
+        when('/about-me', {
+            templateUrl: 'aboutMe/about-me.html',
+            controller: 'aboutMeCtrl'
+        }).
+        otherwise({
+            redirectTo: '/about-me'
+        });
+
+    $locationProvider.html5Mode({
+        enabled: true
+        //requireBase: false
+    })
+}]);
